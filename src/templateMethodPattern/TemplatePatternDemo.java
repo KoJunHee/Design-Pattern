cf79720964ba152a752312ba6365fe16257b9179
jay <kjh338076@naver.com>
2018-05-31 10:37:10 +0900

    template method pattern

---
+++ src/templateMethodPattern/TemplatePatternDemo.java
@@ -0,0 +1,14 @@
+package templateMethodPattern;
+
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
+
+
